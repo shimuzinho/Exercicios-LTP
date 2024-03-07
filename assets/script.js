c92ca9dc10ac7c3ca11ecbfd51450a2da39d3243
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2024-03-07 01:29:34 -0300

    Exercicio1

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,26 @@
-console.log("HELLO WORLD")
+function divisao () {
+  let numeros = pegaNumeros()
+  let saida = document.getElementById("saida")
+  let resultado = numeros[0] / numeros [1]
+
+  try {
+    saida.textContent = numeroValido(resultado)
+  } catch (error){
+    saida.textContent = `Erro: ${error.message}`
+  }
+}
+
+function pegaNumeros() {
+  let inputs = document.querySelectorAll(".numero")
+  let array = []
+  for (numero of inputs) {
+    array.push(parseFloat(numero.value))
+  }
+  return array
+}
+
+function numeroValido (num) {
+  if (isNaN(num)) {
+    throw new Error("A obtenção do resultado não é possível.")
+  }
+}
